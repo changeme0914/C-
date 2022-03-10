a0aabb1b3eec01aa21c749fdd893a258a80b03fa
changeme0914 <1557368148@qq.com>
2022-03-10 21:25:27 +0800

    C++学习第7天

---
+++ 2022.3.10 C++（7）/2022.3.10 C++（7）/2022.3.10.cpp
@@ -0,0 +1,72 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include<iostream>
+//using namespace std;
+//class Student
+//{
+//private:
+//	char name[20];
+//	int sex;
+//public:
+//	void set()
+//	{
+//		cin >> name >> sex;
+//	}
+//	void printf()
+//	{
+//		cout << "Name:" << name << " " << "Score:" << sex<< endl;
+//	}
+//};
+//int main()
+//{
+//	Student a;
+//	a.set();
+//	a.printf();
+//	return 0;
+//}
+//构造函数
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//// Person类
+//class Person 
+//{
+//public:
+//	string name;    // 姓名
+//	int age;    // 年龄
+//
+//	// write your code here......
+//	Person(string n, int a)
+//	{
+//		name = n;
+//		age = a;
+//	}
+//
+//
+//	void showPerson() 
+//	{
+//		cout << name << " " << age << endl;
+//	}
+//};
+//int main()
+//{
+//	string name;
+//	int age;
+//	cin >> name;
+//	cin >> age;
+//	Person p(name, age);
+//	p.showPerson();
+//	return 0;
+//}
+//字符串拼接
+//#include<iostream>
+//#include <string>
+//using namespace std;
+//int main()
+//{
+//	string s1, s2;
+//	getline(cin, s1);
+//	getline(cin, s2);
+//	cout << s1 + s2 << endl;
+//}
+
+
